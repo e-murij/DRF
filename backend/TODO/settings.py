58934ac2cec4ea2c86d7848a27d3bad6dd16e1b0
e.murij <murij.kat@gmail.com>
2022-03-04 16:34:07 +0700

    update views

--- backend/TODO/settings.py
+++ backend/TODO/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'django_filters',
     'corsheaders',
     'usersapp',
     'projectsapp',
@@ -148,7 +149,11 @@ REST_FRAMEWORK = {
         'djangorestframework_camel_case.parser.CamelCaseMultiPartParser',
         'djangorestframework_camel_case.parser.CamelCaseJSONParser',
     ),
-
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend'
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 100,
 }
 
 if DEBUG:
--- backend/usersapp/views.py
+++ backend/usersapp/views.py
@@ -1,8 +1,14 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, UpdateModelMixin
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from .models import User
 from .serializers import UserModelSerializer
 
 
-class UserModelViewSet(ModelViewSet):
+class UserModelViewSet(ListModelMixin, RetrieveModelMixin, UpdateModelMixin, GenericViewSet):
+    """
+        Есть возможность просмотра списка и каждого пользователя в отдельности,
+        можно вносить изменения, нельзя удалять и создавать
+
+    """
     queryset = User.objects.all()
     serializer_class = UserModelSerializer

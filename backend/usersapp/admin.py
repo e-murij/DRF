c8cd9a79a70ffdcbf63f9187e0b0aa3bf4150fe4
e.murij <murij.kat@gmail.com>
2022-03-27 21:36:48 +0700

    change userapp/admin.py

--- backend/usersapp/admin.py
+++ backend/usersapp/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
 from .models import User
 # Register your models here.
-admin.site.register(User)
+
+
+class TODOUserAdmin(UserAdmin):
+    model = User,
+    list_display = ['username', 'first_name', 'last_name', 'email', 'is_staff', 'is_active']
+
+
+admin.site.register(User, TODOUserAdmin)
